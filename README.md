3ce56b7ac4f40f3308710233c64d73d68650809d
Charles Yuan <charlesyuan59@gmail.com>
2022-11-05 20:10:57 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# mary-bot
+<img src="https://i.ytimg.com/vi/9S831972rjA/maxresdefault.jpg">
+
+A Discord bot I created in Go. Her name is Mary. After receiving my offer to work at Uber, I knew I had to learn Go right away, as my interviewer informed me that that was the primary language they used for back end development. I decided to learn some noSQL and MongoDB to implement a database-driven game so that people will actually interact with her (cuz my classmate said that Eve was "so useless"). 
+
+Disclaimer: The images are from an old MMORPG named <a href="https://elsword.koggames.com/">Elsword</a>. I take no credit.
+
+### Prerequisites
+```
+module mary-bot
+
+go 1.19
+
+require (
+	github.com/bwmarrin/discordgo v0.26.1 // direct
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/joho/godotenv v1.4.0 // direct
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
+	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
+)
+```
+
+## License
+This project is licensed under the MIT License - see the <a href="https://github.com/Chubbyman2/mary-bot/blob/main/LICENSE">LICENSE</a> file for details.
---
+++ mary.go
@@ -0,0 +1,66 @@
+package main 
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"github.com/joho/godotenv"
+	"github.com/bwmarrin/discordgo"
+)
+
+func main() {
+	// Load token from env vars
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Printf("Error loading environment variables! %s\n", err)
+		return
+	}
+	TOKEN := os.Getenv("TOKEN")
+	if TOKEN == "" {
+		fmt.Println("Token not found!")
+		return
+	}
+	
+	discord, discordError := discordgo.New("Bot " + TOKEN)
+	if discordError != nil {
+		fmt.Printf("Error creating Discord session! %s\n", discordError)
+		return
+	}
+
+	// Handler for sending messages
+	// Remember to go on Developer Portal, Bot and enable Privileged Gateway Intents (not enabled by default)
+	// https://github.com/bwmarrin/discordgo/issues/1264
+	discord.Identify.Intents = discordgo.IntentMessageContent
+	discord.AddHandler(createMessage)
+	discord.Identify.Intents = discordgo.IntentsGuildMessages
+	
+	err = discord.Open()
+	if err != nil {
+		fmt.Println("Error opening Discord connection!")
+		return
+	}
+	
+	fmt.Println("Mary, online and ready!")
+
+	sc := make(chan os.Signal, 1)
+    signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+    <-sc
+	discord.Close()
+}
+
+func createMessage(session *discordgo.Session, message *discordgo.MessageCreate) {
+	// Ignore all messages sent by Mary herself
+	if message.Author.ID == session.State.User.ID {
+		return
+	}
+
+	fmt.Printf("%s\n", message.Content)
+
+	if message.Content == "test" {
+		_, err := session.ChannelMessageSend(message.ChannelID, "Test successful!")
+		if err != nil {
+			fmt.Printf("Error occurred during testing! %s\n", err)
+		}
+	}
+}
